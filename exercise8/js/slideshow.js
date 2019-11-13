1abf9711d98fa1480ae83af40e8d1b34d0c61dd9
rishabhguptarishi <rishabh@vinsol.com>
2019-11-13 11:46:47 +0530

    exercise8

---
+++ exercise8/js/slideshow.js
@@ -0,0 +1,54 @@
+class Slideshow {
+
+  constructor($slideshowArea) {
+    this.$slideItems = $slideshowArea.find('li');
+    this.slideShowSize = this.$slideItems.length;
+    this.$slideshowArea = $slideshowArea;
+    this.nextItem = 0;
+    this.$navigation = $('<div />', {'class': 'navigation',});
+    this.init();
+  }
+
+  init() {
+    this.$slideshowArea.prependTo('body');
+    this.$navigation.addClass('navigationArea').insertAfter(this.$slideshowArea);
+    this.beginSlideshow();
+  }
+
+  beginSlideshow() {
+    this.$slideItems.first().nextAll().addClass('hidden');
+    this.$navigation.html(`${this.nextItem + 1} of ${this.slideShowSize}... Image: ${this.showImageViewed()}`);
+    this.$slideItems.first().delay(3500).fadeOut(500, () => {
+        this.showNextSlideItem();
+      });
+  }
+
+  showNextSlideItem() {
+    this.nextItem += 1;
+    this.nextItem = this.resetNextItemIfOverflow();
+    this.$navigation.html(`${this.nextItem + 1} of ${this.slideShowSize} --> Image: ${this.showImageViewed()}`);
+    this.revealSlideItem();
+  }
+
+  revealSlideItem() {
+    this.$slideItems.eq(this.nextItem).fadeIn(500).delay(3000).fadeOut(500, () => {
+        this.showNextSlideItem();
+      });
+  }
+
+  resetNextItemIfOverflow() {
+    if (this.nextItem === this.slideShowSize) {
+      this.nextItem = 0;
+    }
+    return this.nextItem;
+  }
+
+  showImageViewed() {
+    return this.$slideItems.eq(this.nextItem).find('img').attr('alt');
+  }
+}
+
+$(() => {
+  let $slideshowArea = $('#slideshow').addClass('slideshowArea');
+  new Slideshow($slideshowArea);
+});
